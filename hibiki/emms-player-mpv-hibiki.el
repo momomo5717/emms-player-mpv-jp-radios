1dc868079b7473439b410eb1cb01d2f7b6928d4e
momomo5717 <momomo5717@users.noreply.github.com>
2015-11-09 23:18:12 +0900

    Update hibiki

--- hibiki/emms-player-mpv-hibiki.el
+++ hibiki/emms-player-mpv-hibiki.el
@@ -42,15 +42,54 @@
 (emms-player-set 'emms-player-mpv-hibiki 'get-media-title
                  'emms-player-mpv-hibiki--get-media-title)
 
-(defun emms-player-mpv-hibiki--data-xml-to-url (data)
-  "Return url from DATA XML."
-  (let* ((protocol (cl-third (assq 'protocol data)))
-         (domain (cl-third (assq 'domain data)))
-         (dir (cl-third (assq 'dir data)))
-         (flv (cl-third (assq 'flv (assq 'channel data))))
-         (mp4 (cl-second (split-string flv ":"))))
-    (unless mp4 (error "Failed to parce DATA XML"))
-    (format "%s://%s/%s/%s" protocol domain dir mp4)))
+(defvar emms-player-mpv-hibiki--base-url-programs
+  "https://vcms-api.hibiki-radio.jp/api/v1/programs/")
+
+(defun emms-player-mpv-hibiki--get-programs-url (access_id)
+  "Return programs url of ACCESS_ID."
+  (format "%s%s"
+          emms-player-mpv-hibiki--base-url-programs access_id))
+
+(defvar emms-player-mpv-hibiki--base-url-play_check
+  "https://vcms-api.hibiki-radio.jp/api/v1/videos/play_check")
+
+(defun emms-player-mpv-hibiki--get-play_check-url (video_id)
+  "Return play_check url of VIDEO_ID."
+  (format "%s?video_id=%s"
+          emms-player-mpv-hibiki--base-url-play_check video_id))
+
+(defun emms-player-mpv-hibiki--url-to-json (url &optional buf)
+  "Return a json object from URL.
+If BUF is no-nil, it is used."
+  (let ((buf (or buf (url-retrieve-synchronously url nil))))
+    (with-current-buffer buf
+      (goto-char (point-min))
+      (while (and (not (eobp)) (not (eolp))) (forward-line 1))
+      (unless (eobp) (forward-line 1))
+      (let ((str (buffer-substring (point) (point-max))))
+             (prog1 (with-temp-buffer
+                      (insert str)
+                      (decode-coding-region (point-min) (point-max) 'utf-8)
+                      (goto-char (point-min))
+                      (json-read))
+               (kill-buffer))))))
+
+(defun emms-player-mpv-hibiki--access_id-to-video_id (access_id)
+  "Return video id of ACCESS_ID ."
+  (let* ((programs (emms-player-mpv-hibiki--url-to-json
+                    (emms-player-mpv-hibiki--get-programs-url access_id)))
+         (id (cdr (assq 'id (assq 'video (assq 'episode programs))))))
+    (unless id
+      (error "Failed to get video id form %S" access_id))
+    id))
+
+(defun emms-player-mpv-hibiki--url-to-playlist_url (url)
+  "Return playlist_url from URL."
+  (let* ((play_check (emms-player-mpv-hibiki--url-to-json url))
+         (playlist_url (cdr (assq 'playlist_url play_check))))
+    (unless playlist_url
+      (error "Failed to get playlist_url from %S" url))
+    playlist_url))
 
 (defun emms-player-mpv-hibiki--loading-message ()
   "Loading message."
@@ -58,20 +97,12 @@
 
 (defun emms-player-mpv-hibiki--track-name-to-input-form (track-name)
   "Return url from TRACK-NAME."
-  (let ((buf (url-retrieve-synchronously
-              (format "http://image.hibiki-radio.jp/uploads/data/channel/%s.xml"
-                      (replace-regexp-in-string "hibiki://" "" track-name)))))
+  (let ((playlist_url (emms-player-mpv-hibiki--url-to-playlist_url
+                       (emms-player-mpv-hibiki--get-play_check-url
+                        (emms-player-mpv-hibiki--access_id-to-video_id
+                         (replace-regexp-in-string "\\`hibiki://" "" track-name))))))
     (later-do 'emms-player-mpv-hibiki--loading-message)
-    (prog1
-        (with-current-buffer buf
-          (goto-char (point-min))
-          (while (and (not (eobp)) (not (eolp))) (forward-line 1))
-          (unless (eobp) (forward-line 1))
-          (let* ((html (libxml-parse-html-region (point) (point-max)))
-                 (body (car (xml-get-children html 'body)))
-                 (data (car (xml-get-children body 'data))))
-            (emms-player-mpv-hibiki--data-xml-to-url data)))
-      (kill-buffer buf))))
+    playlist_url))
 
 (defun emms-player-mpv-hibiki--get-media-title (track)
   "Return media title from TRACK."
--- hibiki/emms-streams-hibiki.el
+++ hibiki/emms-streams-hibiki.el
@@ -26,170 +26,100 @@
 (require 'cl-lib)
 (require 'xml)
 (require 'url)
+(require 'json)
 
 (defvar emms-stream-hibiki--stream-alist-cache
   (cl-loop for i from 1 to 6 collect (list i))
   "Cache for stream alist.")
 
-(cl-defun emms-stream-hibiki--xml-collect-node
-    (name xml-ls &key (test #'identity) (getter #'identity))
-  "Collect nodes of NAME from XML-LS.
-TEST and GETTER takes a node of NAME as an argument.
-TEST is a predicate function.
-Object returned by GETTER is collected."
-  (cl-labels ((collect-name-node (xml-ls ls)
-                (cond
-                 ((atom xml-ls) ls)
-                 ((consp (car xml-ls))
-                  (collect-name-node (car xml-ls)
-                                     (collect-name-node (cdr xml-ls) ls)))
+(defvar emms-stream-hibiki--base-url-programs
+  "https://vcms-api.hibiki-radio.jp/api/v1//programs")
 
-                 ((and (eq (car xml-ls) name)
-                       (funcall test xml-ls))
-                  (cons (funcall getter xml-ls) ls))
-                 ((or (null (car xml-ls))
-                      (not (symbolp (car xml-ls))))
-                  (collect-name-node (cdr xml-ls) ls))
-                 ((symbolp (car xml-ls))
-                  (collect-name-node (xml-node-children xml-ls) ls ))
-                 (t ls))))
-    (collect-name-node xml-ls nil)))
+(defvar emms-stream-hibiki--dow-alist
+  '((1 . "mon") (2 . "tue") (3 . "wed") (4 . "thu") (5 . "fri") (6 . "satsun")))
 
-(defun emms-stream-hibiki--url-to-html (url &optional xmlp buf)
-  (let ((buf (or buf (url-retrieve-synchronously url))))
+(defun emms-stream-hibiki--get-programs-url (dow)
+  "Return programs url of DOW."
+  (format "%s?day_of_week=%s"
+          emms-stream-hibiki--base-url-programs dow))
+
+(defun emms-stream-hibiki--url-to-json (url &optional buf)
+  "Return a json object from URL.
+If BUF is no-nil, it is used."
+  (let ((buf (or buf (url-retrieve-synchronously url nil))))
     (with-current-buffer buf
       (goto-char (point-min))
       (while (and (not (eobp)) (not (eolp))) (forward-line 1))
       (unless (eobp) (forward-line 1))
-      (unwind-protect (funcall (if xmlp #'libxml-parse-xml-region
-                                 #'libxml-parse-html-region) (point) (point-max))
-        (kill-buffer buf)))))
+      (let ((str (buffer-substring (point) (point-max))))
+             (prog1 (with-temp-buffer
+                      (insert str)
+                      (decode-coding-region (point-min) (point-max) 'utf-8)
+                      (goto-char (point-min))
+                      (json-read))
+               (when (buffer-live-p buf)
+                (kill-buffer buf)))))))
 
-(defun emms-stream-hibiki--fetch-description (program-id)
-  "Return description of PROGRAM-ID."
-  (emms-stream-hibiki--url-to-html
-   (format "http://image.hibiki-radio.jp/uploads/data/channel/%s/description.xml"
-           program-id)
-   'xml))
+(defun emms-stream-hibiki--json-program-to-streamlist (program)
+  "Retrun streamlist from json PROGRAM."
+  (let* ((name (cdr (assq 'name program)))
+         (latest_episode_name (cdr (assq 'latest_episode_name program)) )
+         (cast (cdr (assq 'cast program)))
+         (access_id (cdr (assq 'access_id program)))
+         (episode_updated_at (cdr (assq 'episode_updated_at program))))
+    (list (format "%s %s %s: %s"
+                  name latest_episode_name
+                  (if (equal cast "") ""
+                    (format ": %s " cast))
+                  (if (stringp episode_updated_at)
+                      (car (split-string episode_updated_at))
+                    ""))
+          (format "hibiki://%s" access_id)
+          1 'streamlist)))
 
-(defun emms-stream-hibiki--div-hbkProgram-to-streamlist (div-hbkProgram)
-    "Return stream from DIV-HBKPROGRAMS."
-    (let* ((a (car (xml-get-children div-hbkProgram 'a)))
-           (AttachVideo
-            (cdr (split-string (xml-get-attribute a 'onclick) "[()',]" t)))
-           (program-id  (cl-first AttachVideo))
-           (program-key (cl-second AttachVideo))
-           (program-comment
-            (car (emms-stream-hibiki--xml-collect-node
-                  'div a
-                  :test (lambda (node) (equal (xml-get-attribute-or-nil node 'class)
-                                          "hbkProgramComment"))
-                  :getter (lambda (node) (car (xml-node-children node))))))
-           (data (emms-stream-hibiki--fetch-description program-id))
-           (title
-            (car (xml-node-children (car (xml-get-children data 'title)))))
-           (cast
-            (mapcar (lambda (node) (car (xml-node-children node)))
-                    (xml-get-children (car (xml-get-children data 'cast)) 'name)))
-           (cast-names (if cast (concat " : " (mapconcat #'identity cast ", ")) "")))
-      (list (format "%s%s : %s" title cast-names program-comment)
-            (format "hibiki://%s/%s" program-id program-key)
-            1 'streamlist)))
+(defun emms-stream-hibiki--dow-to-stream-list (dow &optional buf)
+  "Return json object for programs of DOW.
+DOW is a number of 1-6.
+If BUF is non-nil, it is used."
+  (let* ((url (emms-stream-hibiki--get-programs-url dow))
+         (programs (emms-stream-hibiki--url-to-json url buf)))
+    (cl-loop for program across programs
+             collect (emms-stream-hibiki--json-program-to-streamlist program))))
 
-(defun emms-stream-hibiki--html-to-stream-list (n html)
-  "Return stream list of HTML."
-  (let ((div-hbkPrograms
-         (emms-stream-hibiki--xml-collect-node
-          'div html
-          :test
-          (lambda (node) (equal (xml-get-attribute-or-nil node 'class)
-                            "hbkProgram")))))
-    (setcdr (assq n emms-stream-hibiki--stream-alist-cache)
-            (mapcar #'emms-stream-hibiki--div-hbkProgram-to-streamlist
-                    div-hbkPrograms))))
-
-(defun emms-stream-hibiki--fetch-stream-list-dow (n &optional updatep)
-  "Retrun stream-list of N\(DOW\).
-If UPDATEP is non-nil, cache is updated."
-  (unless (and (< 0 n) (< n 7)) (error "N needs an integer of 1-6"))
-  (let (stream-list-dow)
+(defun emms-stream-hibiki--fetch-dow-stream-list (dow &optional updatep)
+  "Retrun stream-list of DOW.
+If UPDATEP is non-nil, cache of DOW is updated."
+  (unless (and (< 0 dow) (< dow 7)) (error "DOW needs an integer of 1-6"))
+  (let (dow-stream-list)
     (if (or updatep
-            (null (setq stream-list-dow
-                        (cdr (assq n emms-stream-hibiki--stream-alist-cache)))))
+            (null (setq dow-stream-list
+                        (cdr (assq dow emms-stream-hibiki--stream-alist-cache)))))
         (progn
           (unless emms-stream-hibiki--stream-alist-cache
             (setq emms-stream-hibiki--stream-alist-cache
                   (cl-loop for i from 1 to 6 collect (list i))))
-         (emms-stream-hibiki--html-to-stream-list n
-          (emms-stream-hibiki--url-to-html
-           (format "http://hibiki-radio.jp/get_program/%d" n))))
-      stream-list-dow)))
+          (setcdr
+           (assq dow emms-stream-hibiki--stream-alist-cache)
+           (emms-stream-hibiki--dow-to-stream-list dow)))
+      dow-stream-list)))
 
-(defun emms-stream-hibiki--update-cache-async-1 (n)
-  "Helper function for `emms-stream-hibiki-update-cache-async' for N."
-  (if (and (< 0 n) (< n 7))
+(defun emms-stream-hibiki--update-cache-async-1 (dow)
+  "Update DOW stream-list cache."
+  (if (and (< 0 dow) (< dow 7))
       (url-retrieve
-       (format "http://hibiki-radio.jp/get_program/%d" n)
+       (emms-stream-hibiki--get-programs-url dow)
        (lambda (status &rest _)
          (when (memq :error status)
-           (error "Failed to get hibiki stream list of %s : %s" n (cdr status)))
-         (let* ((html (emms-stream-hibiki--url-to-html
-                       nil nil (current-buffer)))
-                (div-hbkPrograms
-                 (emms-stream-hibiki--xml-collect-node
-                  'div html
-                  :test
-                  (lambda (node) (equal (xml-get-attribute-or-nil node 'class)
-                                    "hbkProgram")))))
-           (emms-stream-hibiki--div-hbkPrograms-to-stream-list-async n div-hbkPrograms)))
-       nil (if (eq n 1) nil t))
+           (error "Failed to get hibiki stream list of %s : %s" dow (cdr status)))
+         (unless emms-stream-hibiki--stream-alist-cache
+           (setq emms-stream-hibiki--stream-alist-cache
+                 (cl-loop for i from 1 to 6 collect (list i))))
+         (setcdr (assq dow emms-stream-hibiki--stream-alist-cache)
+                 (emms-stream-hibiki--dow-to-stream-list nil (current-buffer)))
+         (emms-stream-hibiki--update-cache-async-1 (+ dow 6))))
     (when (cl-loop for ls in emms-stream-hibiki--stream-alist-cache
                    always (cdr ls))
       (message "Updated hibiki stream list cache"))))
-
-(defun emms-stream-hibiki--div-hbkPrograms-to-stream-list-async (n div-hbkPrograms)
-  "Update Nth cache of DIV-HBKPROGRAMS asynchronously."
-  (cl-labels
-      ((update-nth-list (div-hbkPrograms streams)
-        (if div-hbkPrograms
-            (let* ((div-hbkProgram (car div-hbkPrograms))
-                   (a (car (xml-get-children div-hbkProgram 'a)))
-                   (AttachVideo
-                    (cdr (split-string (xml-get-attribute a 'onclick) "[()',]" t)))
-                   (program-id  (cl-first AttachVideo))
-                   (program-key (cl-second AttachVideo))
-                   (program-comment
-                    (car (emms-stream-hibiki--xml-collect-node
-                          'div a
-                          :test (lambda (node) (equal (xml-get-attribute-or-nil node 'class)
-                                                  "hbkProgramComment"))
-                          :getter (lambda (node) (car (xml-node-children node)))))))
-              (url-retrieve
-               (format "http://image.hibiki-radio.jp/uploads/data/channel/%s/description.xml"
-                       program-id)
-               (lambda (status &rest _)
-                 (when (memq :error status)
-                   (error "Failed to get hibiki %s description : %s"
-                          program-id (cdr status)))
-                 (let* ((data (emms-stream-hibiki--url-to-html nil 'xml
-                                                               (current-buffer)))
-                        (title
-                         (car (xml-node-children (car (xml-get-children data 'title)))))
-                        (cast
-                         (mapcar (lambda (node) (car (xml-node-children node)))
-                                 (xml-get-children (car (xml-get-children data 'cast)) 'name)))
-                        (cast-names (if cast (concat " : " (mapconcat #'identity cast ", ")) "")))
-                   (update-nth-list
-                    (cdr div-hbkPrograms)
-                    (cons (list (format "%s%s : %s" title cast-names program-comment)
-                                (format "hibiki://%s/%s" program-id program-key)
-                                1 'streamlist)
-                          streams))))
-               nil t))
-          (setcdr (assq n emms-stream-hibiki--stream-alist-cache)
-                  (nreverse streams))
-          (emms-stream-hibiki--update-cache-async-1 (+ n 6)))))
-    (update-nth-list div-hbkPrograms nil)))
 
 (defun emms-stream-hibiki-update-cache-async ()
   "Update cache asynchronously."
@@ -209,7 +139,7 @@ If UPDATEP is non-nil, cache is updated."
   (let* ((line  (emms-line-number-at-pos (point)))
          (index (+ (/ line 2) 1)))
     (dolist (n nums)
-      (dolist (stream (emms-stream-hibiki--fetch-stream-list-dow n updatep))
+      (dolist (stream (emms-stream-hibiki--fetch-dow-stream-list n updatep))
         (setq emms-stream-list (emms-stream-insert-at index stream
                                                       emms-stream-list))
         (cl-incf index)))
@@ -235,7 +165,7 @@ DOW is a number of 0-6 or -1.
 
 If save,run `emms-stream-save-bookmarks-file' after."
   (interactive "P")
-  (if (eq updatep -1)(emms-stream-hibiki-update-cache-async)
+  (if (eq updatep -1) (emms-stream-hibiki-update-cache-async)
    (unless (integerp dow)
      (let ((msg (concat
                  "[0] All  [1] Mon  [2] Tue  [3] Wed  [4] Thu\n"
