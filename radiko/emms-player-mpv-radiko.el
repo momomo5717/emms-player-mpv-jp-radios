f3b6e9583d92c22823753ec038eaba47b72d0df7
momomo5717 <momomo5717@users.noreply.github.com>
2015-11-28 23:33:55 +0900

    Add emms-stream-radiko-stream-url-to-rtmpe
    Rename emms-player-mpv-radiko--playerurl              emms-stream-radiko--playerurl
    Rename emms-player-mpv-radiko--playerfile             emms-stream-radiko--playerfile
    Rename emms-player-mpv-radiko--keyfile                emms-stream-radiko--keyfile
    Rename emms-player-mpv-radiko--wget-playerfile        emms-stream-radiko--wget-playerfile
    Rename emms-player-mpv-radiko--write-keydata          emms-stream-radiko--write-keydata
    Rename emms-player-mpv-radiko--auth-fms-base-headers  emms-stream-radiko--auth-fms-base-headers
    Rename emms-player-mpv-radiko--access-auth1-fms       emms-stream-radiko--access-auth1-fms
    Rename emms-player-mpv-radiko--get-auth1-value        emms-stream-radiko--get-auth1-value
    Rename emms-player-mpv-radiko--get-authtoken          emms-stream-radiko--get-authtoken
    Rename emms-player-mpv-radiko--get-offset             emms-stream-radiko--get-offset
    Rename emms-player-mpv-radiko--get-length             emms-stream-radiko--get-length
    Rename emms-player-mpv-radiko--get-partialkey         emms-stream-radiko--get-partialkey
    Rename emms-player-mpv-radiko--access-auth2-fms       emms-stream-radiko--access-auth2-fms
    Rename emms-player-mpv-radiko--get-area-id            emms-stream-radiko--get-area-id
    Rename emms-player-mpv-radiko--wget-stream-url        emms-stream-radiko--wget-rtmpe
    
    Add emms-stream-radiko--url-auth1
    Add emms-stream-radiko--url-auth2
    Add emms-stream-radiko--base-url-station-list
    Add emms-stream-radiko--get-area-id-url
    Add emms-stream-radiko--base-url-station-stream
    Add emms-stream-radiko--get-channel-url

--- radiko/emms-player-mpv-radiko.el
+++ radiko/emms-player-mpv-radiko.el
@@ -26,8 +26,8 @@
 ;;; Code:
 (require 'emms-player-simple-mpv)
 (require 'emms-streams)
-(require 'xml)
 (require 'later-do)
+(require 'emms-streams-radiko)
 
 (define-emms-simple-player-mpv mpv-radiko '(streamlist)
   "\\`radiko://"
@@ -44,148 +44,58 @@
 (emms-player-set emms-player-mpv-radiko 'seek nil)
 (emms-player-set emms-player-mpv-radiko 'seek-to nil)
 
-(defvar emms-player-mpv-radiko--playerurl
-  "http://radiko.jp/player/swf/player_4.1.0.00.swf")
-
-(defvar emms-player-mpv-radiko--playerfile
-  (expand-file-name "radiko_player.swf" temporary-file-directory))
-
-(defvar emms-player-mpv-radiko--keyfile
-  (expand-file-name "radiko_authkey.png" temporary-file-directory))
-
-(defun emms-player-mpv-radiko--wget-playerfile ()
-  "Wget player.swf to `emms-player-mpv-radiko--playerfile'."
-  (unless (file-exists-p emms-player-mpv-radiko--playerfile)
-    (unless (zerop (call-process-shell-command
-                    (format "wget -q -O %s %s"
-                            emms-player-mpv-radiko--playerfile
-                            emms-player-mpv-radiko--playerurl)))
-      (error "Failed to wget the player.swf"))))
-
-(defun emms-player-mpv-radiko--write-keydata ()
-  "Write keydata from emms-radiko-keyfile."
-  (unless (file-exists-p emms-player-mpv-radiko--keyfile)
-    (unless (zerop (call-process-shell-command
-                    (format "swfextract -b 14 %s -o %s"
-                            emms-player-mpv-radiko--playerfile
-                            emms-player-mpv-radiko--keyfile)))
-      (error "Failed to write the keydata"))))
-
-(defvar emms-player-mpv-radiko--auth-fms-base-headers
-  '(("pragma" . "no-cache")
-    ("X-Radiko-App" . "pc_1")
-    ("X-Radiko-App-Version" . "2.0.1")
-    ("X-Radiko-User" . "test-stream")
-    ("X-Radiko-Device" . "pc"))
-  "Base headers for auth fms.")
-
-(defun emms-player-mpv-radiko--access-auth1-fms ()
-  "Return auth1_fms."
-  (let* ((url-request-method "POST")
-         (url-request-data "\\r\\n")
-         (url-request-extra-headers
-          emms-player-mpv-radiko--auth-fms-base-headers)
-         (buf (url-retrieve-synchronously
-               "https://radiko.jp/v2/api/auth1_fms" t)))
-    (with-current-buffer buf
-      (prog1 (buffer-substring-no-properties
-              (point-min) (point-max))
-        (kill-buffer buf)))))
-
-(defun emms-player-mpv-radiko--get-auth1-value (key auth1)
-  "Return value of KEY from AUTH1."
-  (if (string-match (format "%s: " key) auth1)
-      (let ((pos (match-end 0)))
-        (if (string-match "\n\\|\r\n" auth1 pos)
-            (substring auth1 pos (match-beginning 0))
-          (error "Failed to get %s value" key)))
-    (error "Failed to get %s value" key)))
-
-(defun emms-player-mpv-radiko--get-authtoken (auth1)
-  "Return X-Radiko-Authtoken value from  AUTH1."
-  (emms-player-mpv-radiko--get-auth1-value "X-Radiko-AuthToken" auth1))
-
-(defun emms-player-mpv-radiko--get-offset (auth1)
-  "Return x-radiko-keyoffset value from  AUTH1."
-  (emms-player-mpv-radiko--get-auth1-value "X-Radiko-KeyOffset" auth1))
-
-(defun emms-player-mpv-radiko--get-length (auth1)
-  "Return x-radiko-keylength value from  AUTH1."
-  (emms-player-mpv-radiko--get-auth1-value "X-Radiko-KeyLength" auth1))
-
-(defun emms-player-mpv-radiko--get-partialkey (keyfile auth1)
-  "Return partialkey from KEYFILE, AUTH1."
-  (car (split-string
-        (shell-command-to-string
-         (format "dd if=%s bs=1 skip=%s count=%s 2> /dev/null | base64"
-                 keyfile
-                 (emms-player-mpv-radiko--get-offset auth1)
-                 (emms-player-mpv-radiko--get-length auth1)))
-        "\n")))
-
-(defun emms-player-mpv-radiko--access-auth2-fms (auth1)
-  "Return auth2_fms from AUTH1."
-  (let* ((url-request-method "POST")
-         (url-request-data "\\r\\n")
-         (url-request-extra-headers
-          `(("X-Radiko-Authtoken" .
-             ,(emms-player-mpv-radiko--get-authtoken auth1))
-            ("X-Radiko-Partialkey" .
-             ,(emms-player-mpv-radiko--get-partialkey
-               emms-player-mpv-radiko--keyfile auth1))
-            ,@emms-player-mpv-radiko--auth-fms-base-headers))
-         (buf (url-retrieve-synchronously
-               "https://radiko.jp/v2/api/auth2_fms" t)))
-    (with-current-buffer buf
-      (prog1 (buffer-substring-no-properties
-              (point-min) (point-max))
-        (kill-buffer buf)))))
-
-(defun emms-player-mpv-radiko--get-area-id (auth2)
-  "Retrun area-id from AUTH2."
-  (if (string-match "^JP[0-9]+[,]" auth2)
-      (substring auth2 (match-beginning 0) (1- (match-end 0)))
-    (error "Failed to get area-id")))
-
-(defun emms-player-mpv-radiko--wget-stream-url (channel)
-  "Return stream-url from CHANNEL.
-\"channel\"
-=> \"rtmpe://f-radiko.smartstream.ne.jp/channel/_definst_/simul-stream.stream\""
-  (let ((buf (url-retrieve-synchronously
-              (format "http://radiko.jp/v2/station/stream/%s.xml" channel) t))
-        (stream-url nil))
-    (with-current-buffer buf
-      (goto-char (point-min))
-      (while (and (not (eobp)) (not (eolp))) (forward-line 1))
-      (unless (eobp) (forward-line 1))
-      (let* ((stream-xml (libxml-parse-xml-region (point) (point-max)))
-             (item (car (xml-get-children stream-xml 'item))))
-        (setq stream-url (car (xml-node-children item))))
-      (kill-buffer buf))
-    (if (stringp stream-url) stream-url
-      (error "Failed to parce stream/%s.xml" channel))))
-
 (defun emms-player-mpv-radiko--loading-message ()
   "Loading message."
   (message "Loading Radiko ... It takes a few seconds."))
 
 (defun emms-player-mpv-radiko--track-name-to-input-form (track-name)
   "Retrun \"rtmpe://...\" ffplay format from TRACK-NAME."
-  (emms-player-mpv-radiko--wget-playerfile)
-  (emms-player-mpv-radiko--write-keydata)
-  (let* ((channel (replace-regexp-in-string "\\`radiko://" "" track-name))
-         (rtmpe-url (emms-player-mpv-radiko--wget-stream-url channel))
-         (auth1 (emms-player-mpv-radiko--access-auth1-fms))
-         (authtoken (emms-player-mpv-radiko--get-authtoken auth1)))
-    (emms-player-mpv-radiko--access-auth2-fms auth1)
+  (emms-stream-radiko--wget-playerfile)
+  (emms-stream-radiko--write-keydata)
+  (let* ((rtmpe (emms-stream-radiko-stream-url-to-rtmpe track-name))
+         (auth1 (emms-stream-radiko--access-auth1-fms))
+         (authtoken (emms-stream-radiko--get-authtoken auth1)))
+    (emms-stream-radiko--access-auth2-fms auth1)
     (later-do 'emms-player-mpv-radiko--loading-message)
     (format "%s swfUrl=%s swfVfy=1 conn=S:  conn=S:  conn=S:  conn=S:%s live=1"
-            rtmpe-url emms-player-mpv-radiko--playerurl authtoken)))
+            rtmpe emms-stream-radiko--playerurl authtoken)))
 
 (defun emms-player-mpv-radiko--get-media-title (track)
   (if (eq (emms-track-type track) 'streamlist)
       (emms-stream-name (emms-track-get track 'metadata))
     (file-name-nondirectory (emms-track-name track))))
+
+
+(define-obsolete-variable-alias 'emms-player-mpv-radiko--playerurl
+  'emms-stream-radiko--playerurl "20151128")
+(define-obsolete-variable-alias 'emms-player-mpv-radiko--playerfile
+  'emms-stream-radiko--playerfile "20151128")
+(define-obsolete-variable-alias 'emms-player-mpv-radiko--keyfile
+  'emms-stream-radiko--keyfile "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--wget-playerfile
+  'emms-stream-radiko--wget-playerfile "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--write-keydata
+  'emms-stream-radiko--write-keydata "20151128")
+(define-obsolete-variable-alias 'emms-player-mpv-radiko--auth-fms-base-headers
+  'emms-stream-radiko--auth-fms-base-headers "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--access-auth1-fms
+  'emms-stream-radiko--access-auth1-fms "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--get-auth1-value
+  'emms-stream-radiko--get-auth1-value "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--get-authtoken
+  'emms-stream-radiko--get-authtoken "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--get-offset
+  'emms-stream-radiko--get-offset "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--get-length
+  'emms-stream-radiko--get-length "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--get-partialkey
+  'emms-stream-radiko--get-partialkey "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--access-auth2-fms
+  'emms-stream-radiko--access-auth2-fms "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--get-area-id
+  'emms-stream-radiko--get-area-id "20151128")
+(define-obsolete-function-alias 'emms-player-mpv-radiko--wget-stream-url
+  'emms-stream-radiko--wget-rtmpe "20151128")
 
 (provide 'emms-player-mpv-radiko)
 ;;; emms-player-mpv-radiko.el ends here
--- radiko/emms-streams-radiko.el
+++ radiko/emms-streams-radiko.el
@@ -27,15 +27,132 @@
 (require 'cl-lib)
 (require 'xml)
 (require 'url)
-(require 'emms-player-mpv-radiko)
+
+(defvar emms-stream-radiko--playerurl
+  "http://radiko.jp/player/swf/player_4.1.0.00.swf")
+
+(defvar emms-stream-radiko--playerfile
+  (expand-file-name "radiko_player.swf" temporary-file-directory))
+
+(defvar emms-stream-radiko--keyfile
+  (expand-file-name "radiko_authkey.png" temporary-file-directory))
+
+(defvar emms-stream-radiko--url-auth1
+  "https://radiko.jp/v2/api/auth1_fms")
+
+(defvar emms-stream-radiko--url-auth2
+  "https://radiko.jp/v2/api/auth2_fms")
+
+(defun emms-stream-radiko--wget-playerfile ()
+  "Wget player.swf to `emms-stream-radiko--playerfile'."
+  (unless (file-exists-p emms-stream-radiko--playerfile)
+    (unless (zerop (call-process-shell-command
+                    (format "wget -q -O %s %s"
+                            emms-stream-radiko--playerfile
+                            emms-stream-radiko--playerurl)))
+      (error "Failed to wget the player.swf"))))
+
+(defun emms-stream-radiko--write-keydata ()
+  "Write keydata from emms-radiko-keyfile."
+  (unless (file-exists-p emms-stream-radiko--keyfile)
+    (unless (zerop (call-process-shell-command
+                    (format "swfextract -b 14 %s -o %s"
+                            emms-stream-radiko--playerfile
+                            emms-stream-radiko--keyfile)))
+      (error "Failed to write the keydata"))))
+
+(defvar emms-stream-radiko--auth-fms-base-headers
+  '(("pragma" . "no-cache")
+    ("X-Radiko-App" . "pc_1")
+    ("X-Radiko-App-Version" . "2.0.1")
+    ("X-Radiko-User" . "test-stream")
+    ("X-Radiko-Device" . "pc"))
+  "Base headers for auth fms.")
+
+(defun emms-stream-radiko--access-auth1-fms ()
+  "Return auth1_fms."
+  (let* ((url-request-method "POST")
+         (url-request-data "\\r\\n")
+         (url-request-extra-headers
+          emms-stream-radiko--auth-fms-base-headers)
+         (buf (url-retrieve-synchronously
+               emms-stream-radiko--url-auth1 t)))
+    (with-current-buffer buf
+      (prog1 (buffer-substring-no-properties
+              (point-min) (point-max))
+        (kill-buffer buf)))))
+
+(defun emms-stream-radiko--get-auth1-value (key auth1)
+  "Return value of KEY from AUTH1."
+  (if (string-match (format "%s: " key) auth1)
+      (let ((pos (match-end 0)))
+        (if (string-match "\n\\|\r\n" auth1 pos)
+            (substring auth1 pos (match-beginning 0))
+          (error "Failed to get %s value" key)))
+    (error "Failed to get %s value" key)))
+
+(defun emms-stream-radiko--get-authtoken (auth1)
+  "Return X-Radiko-Authtoken value from  AUTH1."
+  (emms-stream-radiko--get-auth1-value "X-Radiko-AuthToken" auth1))
+
+(defun emms-stream-radiko--get-offset (auth1)
+  "Return x-radiko-keyoffset value from  AUTH1."
+  (emms-stream-radiko--get-auth1-value "X-Radiko-KeyOffset" auth1))
+
+(defun emms-stream-radiko--get-length (auth1)
+  "Return x-radiko-keylength value from  AUTH1."
+  (emms-stream-radiko--get-auth1-value "X-Radiko-KeyLength" auth1))
+
+(defun emms-stream-radiko--get-partialkey (keyfile auth1)
+  "Return partialkey from KEYFILE, AUTH1."
+  (car (split-string
+        (shell-command-to-string
+         (format "dd if=%s bs=1 skip=%s count=%s 2> /dev/null | base64"
+                 keyfile
+                 (emms-stream-radiko--get-offset auth1)
+                 (emms-stream-radiko--get-length auth1))))))
+
+(defun emms-stream-radiko--access-auth2-fms (auth1)
+  "Return auth2_fms from AUTH1."
+  (let* ((url-request-method "POST")
+         (url-request-data "\\r\\n")
+         (url-request-extra-headers
+          `(("X-Radiko-Authtoken" .
+             ,(emms-stream-radiko--get-authtoken auth1))
+            ("X-Radiko-Partialkey" .
+             ,(emms-stream-radiko--get-partialkey
+               emms-stream-radiko--keyfile auth1))
+            ,@emms-stream-radiko--auth-fms-base-headers))
+         (buf (url-retrieve-synchronously
+               emms-stream-radiko--url-auth2 t)))
+    (with-current-buffer buf
+      (prog1 (buffer-substring-no-properties
+              (point-min) (point-max))
+        (kill-buffer buf)))))
+
+(defun emms-stream-radiko--get-area-id (auth2)
+  "Retrun area-id from AUTH2."
+  (if (string-match "^JP[0-9]+[,]" auth2)
+      (substring auth2 (match-beginning 0) (1- (match-end 0)))
+    (error "Failed to get area-id")))
+
+;; For stream-list
 
 (defvar emms-stream-radiko-stream-list-cache nil)
+
+(defvar emms-stream-radiko--base-url-station-list
+  "http://radiko.jp/v2/station/list/")
+
+(defun emms-stream-radiko--get-area-id-url (area-id)
+  "Return AREA-ID link."
+  (format "%s%s.xml" emms-stream-radiko--base-url-station-list area-id))
+
 (defun emms-stream-radiko--fetch-stream-list (area-id &optional buf)
   "Retrun AREA-ID radiko stream list.
 string -> stream-list
 \(emms-radiko-wget-radiko-stream-list \"JP13\"\) => stream-list"
   (let ((buf (or buf (url-retrieve-synchronously
-                      (format "http://radiko.jp/v2/station/list/%s.xml" area-id)))))
+                      (emms-stream-radiko--get-area-id-url area-id)))))
     (with-current-buffer buf
       (goto-char (point-min))
       (while (and (not (eobp)) (not (eolp))) (forward-line 1))
@@ -55,50 +172,50 @@ string -> stream-list
       emms-stream-radiko-stream-list-cache
     (setq emms-stream-radiko-stream-list-cache
           (progn
-            (emms-player-mpv-radiko--wget-playerfile)
-            (emms-player-mpv-radiko--write-keydata)
+            (emms-stream-radiko--wget-playerfile)
+            (emms-stream-radiko--write-keydata)
             (emms-stream-radiko--fetch-stream-list
-             (emms-player-mpv-radiko--get-area-id
-              (emms-player-mpv-radiko--access-auth2-fms
-               (emms-player-mpv-radiko--access-auth1-fms))))))))
+             (emms-stream-radiko--get-area-id
+              (emms-stream-radiko--access-auth2-fms
+               (emms-stream-radiko--access-auth1-fms))))))))
 
 (defun emms-stream-radiko--wget-playerfile-async (&optional cont)
   "Run CONT with no arguments after doing wget playerfile."
-  (if (file-exists-p emms-player-mpv-radiko--playerfile)
+  (if (file-exists-p emms-stream-radiko--playerfile)
       (when (functionp cont) (funcall cont))
     (cl-labels
         ((wget-playerfile-async-filter (proc _)
           (let ((ps (process-status proc)))
             (when (eq ps 'exit)
-              (unless (file-exists-p emms-player-mpv-radiko--playerfile)
-                (error "Failed to get %s" emms-player-mpv-radiko--playerfile))
+              (unless (file-exists-p emms-stream-radiko--playerfile)
+                (error "Failed to get %s" emms-stream-radiko--playerfile))
               (when (functionp cont) (funcall cont))))))
       (set-process-sentinel
        (start-process "radiko-wget-playerfile-async"
                       nil
                       "wget" "-q" "-O"
-                      emms-player-mpv-radiko--playerfile
-                      emms-player-mpv-radiko--playerurl)
+                      emms-stream-radiko--playerfile
+                      emms-stream-radiko--playerurl)
        #'wget-playerfile-async-filter))))
 
 (defun emms-stream-radiko--write-keydata-async (&optional cont)
   "Run CONT after writing keydata."
-  (if (file-exists-p  emms-player-mpv-radiko--keyfile)
+  (if (file-exists-p  emms-stream-radiko--keyfile)
       (when (functionp cont) (funcall cont))
     (cl-labels
         ((write-keydate-async-filter (proc _)
           (let ((ps (process-status proc)))
             (when (eq ps 'exit)
-              (unless (file-exists-p emms-player-mpv-radiko--keyfile)
-                (error "Failed to write %s" emms-player-mpv-radiko--keyfile))
+              (unless (file-exists-p emms-stream-radiko--keyfile)
+                (error "Failed to write %s" emms-stream-radiko--keyfile))
               (when (functionp cont) (funcall cont))))))
      (set-process-sentinel
       (start-process "radiko-write-keydata-async"
                      nil
                      "swfextract" "-b" "14"
-                     emms-player-mpv-radiko--playerfile
+                     emms-stream-radiko--playerfile
                      "-o"
-                     emms-player-mpv-radiko--keyfile)
+                     emms-stream-radiko--keyfile)
       #'write-keydate-async-filter))))
 
 (defun emms-stream-radiko--access-auth1-fms-async (&optional cont)
@@ -106,10 +223,10 @@ string -> stream-list
   (let ((url-request-method "POST")
         (url-request-data "\\r\\n")
         (url-request-extra-headers
-          emms-player-mpv-radiko--auth-fms-base-headers)
+         emms-stream-radiko--auth-fms-base-headers)
         (auth1 ""))
     (url-retrieve
-     "https://radiko.jp/v2/api/auth1_fms"
+     emms-stream-radiko--url-auth1
      (lambda (status &rest _)
        (when (memq :error status)
          (error "Failed to get auth1_fms : %s" (cdr status)))
@@ -125,26 +242,26 @@ string -> stream-list
         (url-request-data "\\r\\n")
         (url-request-extra-headers
          `(("X-Radiko-Authtoken" .
-            ,(emms-player-mpv-radiko--get-authtoken auth1))
+            ,(emms-stream-radiko--get-authtoken auth1))
            ("X-Radiko-Partialkey" .
-            ,(emms-player-mpv-radiko--get-partialkey
-              emms-player-mpv-radiko--keyfile auth1))
-           ,@emms-player-mpv-radiko--auth-fms-base-headers))
+            ,(emms-stream-radiko--get-partialkey
+              emms-stream-radiko--keyfile auth1))
+           ,@emms-stream-radiko--auth-fms-base-headers))
         (auth2 ""))
     (url-retrieve
-     "https://radiko.jp/v2/api/auth2_fms"
+     emms-stream-radiko--url-auth2
      (lambda (status &rest _)
        (when (memq :error status)
          (error "Failed to get auth1_fms : %s" (cdr status)))
        (setq auth2 (buffer-substring-no-properties
                     (point-min) (point-max)))
-                     (kill-buffer)
-              (funcall cont auth2)))))
+       (kill-buffer)
+       (funcall cont auth2)))))
 
 (defun emms-stream-radiko-update-cache-async-1 (area-id)
   "Helper function for `emms-stream-radiko-update-cache-async'."
   (url-retrieve
-   (format "http://radiko.jp/v2/station/list/%s.xml" area-id)
+   (emms-stream-radiko--get-area-id-url area-id)
    (lambda (status &rest _)
      (when (memq :error status)
        (error "Failed to get radiko station list : %s" (cdr status)))
@@ -163,7 +280,7 @@ string -> stream-list
            (emms-stream-radiko--access-auth2-fms-async
             auth1 (lambda (auth2)
                     (emms-stream-radiko-update-cache-async-1
-                     (emms-player-mpv-radiko--get-area-id auth2))))))))
+                     (emms-stream-radiko--get-area-id auth2))))))))
     (emms-stream-radiko--wget-playerfile-async
      (lambda ()
        (emms-stream-radiko--write-keydata-async
@@ -196,6 +313,38 @@ If save,run `emms-stream-save-bookmarks-file' after."
       (emms-stream-redisplay)
       (goto-char (point-min))
       (forward-line (1- line)))))
+
+;; For media player
+
+(defvar emms-stream-radiko--base-url-station-stream
+  "http://radiko.jp/v2/station/stream/")
+
+(defun emms-stream-radiko--get-channel-url (channel)
+  "Return CHANNEL link."
+  (format "%s%s.xml" emms-stream-radiko--base-url-station-stream channel))
+
+(defun emms-stream-radiko--wget-rtmpe (channel)
+  "Return stream-url from CHANNEL.
+\"channel\"
+=> \"rtmpe://f-radiko.smartstream.ne.jp/channel/_definst_/simul-stream.stream\""
+  (let ((buf (url-retrieve-synchronously
+              (emms-stream-radiko--get-channel-url  channel) t))
+        (rtmpe nil))
+    (with-current-buffer buf
+      (goto-char (point-min))
+      (while (and (not (eobp)) (not (eolp))) (forward-line 1))
+      (unless (eobp) (forward-line 1))
+      (let* ((stream-xml (libxml-parse-xml-region (point) (point-max)))
+             (item (car (xml-get-children stream-xml 'item))))
+        (setq rtmpe (car (xml-node-children item))))
+      (kill-buffer buf))
+    (if (stringp rtmpe) rtmpe
+      (error "Failed to parse stream/%s.xml" channel))))
+
+(defun emms-stream-radiko-stream-url-to-rtmpe (stream-url)
+  "Return rtmpe from STREAM-URL."
+  (emms-stream-radiko--wget-rtmpe
+   (replace-regexp-in-string "\\`radiko://" "" stream-url)))
 
 (provide 'emms-streams-radiko)
 ;;; emms-streams-radiko.el ends here
